d5927bcad7c47979e8ec8d23d6363e6fc4748674
hyetigran <tigran.asriyan@gmail.com>
2020-05-07 19:55:41 -0400

    add output list of places

---
+++ screens/PlaceDetailScreen.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const PlaceDetailScreen = (props) => {
+  return (
+    <View>
+      <Text>Places List Screen </Text>
+    </View>
+  );
+};
+
+PlaceDetailScreen.navigationOptions = (navData) => {
+  return {
+    headerTitle: navData.navigation.getParam("placeTitle"),
+  };
+};
+const styles = StyleSheet.create({});
+export default PlaceDetailScreen;
